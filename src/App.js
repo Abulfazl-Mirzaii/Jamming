3160c5142715e91e63d9e2e832e3a774acc0c5fc
Abulfazl Mirzaii <Mirzaii.Abulfazl@gmail.com>
2024-05-12 20:26:54 +0430

    needed JavaScript files added to the staging area, and the App.js modified for functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import Playlist from './Playlist';
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
+import Track from './Track';
+import Tracklist from './Tracklist';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
 }
 
 export default App;
